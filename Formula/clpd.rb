de6b76f688756d9899ca95c65b4cfb88eeeabb64
Merlot Liu <75664598+merlotliu@users.noreply.github.com>
2025-07-09 17:16:39 +0800

    update sha256

--- Formula/clpd.rb
+++ Formula/clpd.rb
@@ -5,10 +5,10 @@ class Clpd < Formula
 
   if OS.mac?
     url "https://gitlab.chehejia.com/liumoulu/clpd/uploads/37e1ac550b0e1ae37ec83d6f7390c885/clpd-darwin-amd64.tar.gz"
-    sha256 "604bbb17ed701e844b0f22a5e271e83bce60aa1c2b301986c5afc1d8a3261943"
+    sha256 "d016e3a7a4d9ffad59b8df665b03d2cb028f379c87049252a851cf4bdc633182"
   elsif OS.linux?
     url "https://gitlab.chehejia.com/liumoulu/clpd/uploads/e1b865c9ffe239af013c80831e9d6dbe/clpd-linux-amd64.tar.gz"
-    sha256 "d016e3a7a4d9ffad59b8df665b03d2cb028f379c87049252a851cf4bdc633182"
+    sha256 "604bbb17ed701e844b0f22a5e271e83bce60aa1c2b301986c5afc1d8a3261943"
   end
 
   def install
